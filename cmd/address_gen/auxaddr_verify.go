070f4455b80f6541c904abcd5ac6ca8315d5b2a9
devzone777 <50347092+devzone777@users.noreply.github.com>
2021-01-02 17:32:38 -0500

    Create auxaddr_verify.go

---
+++ cmd/address_gen/auxaddr_verify.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+        "fmt"
+        "os"
+        "net/http"
+)
+
+func main() {
+        //declare address to be verified
+
+        //get address from os.Args
+
+        //call Skycoin explorer 
+
+        //verify address returns valid
+}
