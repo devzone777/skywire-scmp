c47ae0b037020c03f5e37021cb3a411b8b924b44
root <fivestarg777@yahoo.com>
2021-01-31 12:54:51 -0500

    add arg to init script

--- cmd/skywire-connect-init.sh
+++ cmd/skywire-connect-init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go run ~/skywire-scmp/cmd/skywire-connect.go Dx5nqQpRidueMTquwgornCrrJHatqWKp & pkill wpa_supplicant & screen -dmS skywire-wpa-supplicant wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
+go run ~/skywire-scmp/cmd/skywire-connect.go "$1" & pkill wpa_supplicant & screen -dmS skywire-wpa-supplicant wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
